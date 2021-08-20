2b65755018d08a20d5fa811d35d6512da799b41f
mateus <mateus@turris.dev>
2021-08-20 09:24:27 -0300

    Meu primeiro teste

---
+++ APIComprasTest/CompraControllerTest.cs
@@ -0,0 +1,36 @@
+﻿using APICompras.Controllers;
+using APICompras.Models;
+using APICompras.Services;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using Xunit;
+namespace APIComprasTest
+{
+    public class CompraControllerTest
+    {
+        CompraController _controller;
+        ICompraService _service;
+        public CompraControllerTest()
+        {
+            _service = new CompraServiceFake();
+            _controller = new CompraController(_service);
+        }
+        [Fact]
+        public void Get_WhenCalled_ReturnsOkResult()
+        {
+            // Act
+            var okResult = _controller.Get();
+            // Assert
+            Assert.IsType<OkObjectResult>(okResult.Result);
+        }
+        [Fact]
+        public void Get_WhenCalled_ReturnsAllItems()
+        {
+            // Act
+            var okResult = _controller.Get().Result as OkObjectResult;
+            // Assert
+            var items = Assert.IsType<List<CompraItem>>(okResult.Value);
+            Assert.Equal(5, items.Count);
+        }
+    }
+}
